1602341dc42e0a111fb03061a3684ec783f8dcc0
durgesh <dkg.2395@gmail.com>
2022-11-08 11:21:20 +0530

    var added

--- src/main/java/com/durgesh/GitApplication.java
+++ src/main/java/com/durgesh/GitApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitApplication {
+	int a=10;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitApplication.class, args);
